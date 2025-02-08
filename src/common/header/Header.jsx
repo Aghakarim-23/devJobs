07ee43837e892c86f5a1229d1a76a333d21eec38
Aghakarim-dev <hemidzade1010@gmail.com>
2025-02-08 22:12:57 +0400

    bg photo changed

---
+++ src/common/header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className="
+    h-[136px] mb-4
+                     bg-[url('assets/mobileBg.png')] bg-no-repeat 
+                  md:bg-[url('assets/tabletBg.png')] md:bg-contain md:h-[160px]
+                  lg:bg-[url('assets/desktopBg.png')] lg:h-[162px]">
+        
+    </div>
+  )
+}
+
+export default Header
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import userData  from "../../data/data.js";
 import UserCard from "../../common/userCard/UserCard.jsx";
+import Header from "../../common/header/Header.jsx";
 
 const Home = () => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12 p-10">
-      {userData.map((user, index) => (
-        <UserCard key={index} userData={user} />
-      ))}
+    <div>
+      <Header/>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12 p-10">
+        {userData.map((user, index) => (
+          <UserCard key={index} userData={user} />
+        ))}
+    </div>
     </div>
   );
 };
